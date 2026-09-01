73f2452f40788ba59db206f9aebf2d5bc6548add
Aakash Arora <aakasharora.work@gmail.com>
2026-09-01 20:36:30 +0100

    Chore: Clean up patch scripts

--- patch_nulls.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('app.js', 'utf8');
-
-code = code.replace(/document\.getElementById\('play'\)\.classList/g, "(document.getElementById('play') || document.body).classList");
-
-fs.writeFileSync('app.js', code);
